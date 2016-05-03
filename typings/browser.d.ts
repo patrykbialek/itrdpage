2074f61fde40e0be5cf0f6e5f9ba5fc0773f439f
Patryk Białek <patryk@MacBook-Pro-Patryk.local>
2016-05-03 11:04:48 +0200

    Configuration vscod

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="browser/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="browser/ambient/jasmine/jasmine.d.ts" />
 /// <reference path="browser\ambient\es6-shim\es6-shim.d.ts" />
 /// <reference path="browser\ambient\jasmine\jasmine.d.ts" />
